564a5187aae9475255127480ac01e7fb19e2c086
popopuchang <popopuchang@gmail.com>
2022-10-22 10:18:10 +0800

    10/22

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pygame
-pygame-1/-2: 打蚊子
+
+#等待事件发生
+    # event = pygame.event.wait()
+    # if event.type == pygame.QUIT:
+    #     exit()
+    # if event.type == pygame.MOUSEBUTTONDOWN:
+    #     print('鼠标按下',event.pos)
+    # if event.type == pygame.MOUSEBUTTONUP:
+    #     print('鼠标弹起')
+    # if event.type == pygame.MOUSEMOTION:
+    #     print('鼠标移动')
+    #     # 键盘事件
+    # if event.type ==pygame.KEYDOWN:
+    #     # 打印按键的英文字符
+    #     print('键盘按下',chr(event.key))
+    # if event.type == pygame.KEYUP:
+    #     print('键盘弹起')
+
+    site = [0, 0]
+    # 移动图像
+    position = position.move(site)
--- pygame-3.py
+++ pygame-3.py
@@ -38,10 +38,33 @@ def main():
     # image_1 =pygame.transform.rotate(image_new,45)
     # 使用rotozoom() 旋转 0 度，将图像缩小0.5倍
     # image_2 = pygame.transform.rotozoom(image_1,0,0.5)
+    
+    # 获取以毫秒为单位的时间
+    t = pygame.time.get_ticks() #该时间指的从pygame初始化后开始计算，到调用该函数为止
+    t1 =pygame.time.wait(3000) #暂停游戏3000毫秒
+    print(t1)
 
+
+    image_surface = pygame.image.load("../1.jpg")
+    rect1 = pygame.Rect(50,50,100,100)
+    # 在原图的基础上创建一个新的子图（surface对象）
+    image_child= image_surface.subsurface(rect1)
+    rect2 = image_child.get_rect()
+    #输出的矩形大小为 100*100
+    print(rect2)
+
+
+
+    #创建时钟对象（控制游戏的FPS）
+    clock = pygame.time.Clock()
 
     #game loop
     while True:
+
+        #通过时钟对象，指定循环频率，每秒循环60次
+        clock.tick(60)
+
+
         #listening event status
         for event in pygame.event.get():
             #if X
@@ -51,13 +74,18 @@ def main():
                 # end process
                 sys.exit()
         #add face to screen
-        screen.blit(face, (100,100))
+        screen.blit(image_child, (100,100))
 
         #refresh all screen
         pygame.display.flip()
 
         #可以根据选定的区域来更新部分内容，如果没有提供区域位置参数时，其作用和 display.flip() 相同
         # pygame.display.update() 
+
+
+
+
+
 
 
 
